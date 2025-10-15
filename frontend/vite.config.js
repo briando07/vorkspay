883b1168d6d65584d8667611595a1eb0986c20d6
briando07 <briandouglas959@gmail.com>
2025-10-15 05:55:26 -0300

    Adiciona frontend React com integração ao backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], server: { port: 3000, proxy: {'/api':'http://localhost:3333'} } })
+export default defineConfig({ plugins: [react()], server: { port: 8080, proxy: {'/api':'vorkspay-production.up.railway.app'} } })
